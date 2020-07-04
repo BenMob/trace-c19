9a99f16070e49ff469231d15e6e58017c601009a
Ben <benjaminombeni3@gmail.com>
2020-07-04 03:09:01 -0700

    Fix some configuration issues

---
+++ trace-C19/src/main/java/com/tracecovid/servlets/incomingLocationServlet.java
@@ -0,0 +1,21 @@
+package com.tracecovid.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/inLocation")
+public class incomingLocationServlet extends HttpServlet{
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        
+    }
+}
