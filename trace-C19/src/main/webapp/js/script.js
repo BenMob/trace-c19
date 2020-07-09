b663723e3c97654ba62c9876a7aeec63f7dbc05e
Ben <benjaminombeni3@gmail.com>
2020-07-09 13:51:19 -0700

    Create fake forms just for testing purposes

---
+++ trace-C19/src/main/webapp/js/script.js
@@ -0,0 +1,4 @@
+/*
+TODO: Use fetch() to send a POST request at "/outLocation" with a ZIP Code as paramter and 
+you should reive a Json Object with all the locations (lat/long) matching that zip code.
+*/
