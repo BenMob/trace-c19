159931ba6efd4cbc97267c916442e763df573c8d
Ben <benjaminombeni3@gmail.com>
2020-07-09 13:49:55 -0700

    Adds call locations service methods to process servlets data

--- trace-C19/src/main/java/com/tracecovid/servlets/IncomingLocationServlet.java
+++ trace-C19/src/main/java/com/tracecovid/servlets/IncomingLocationServlet.java
@@ -5,17 +5,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.tracecovid.services.LocationService;
+import com.google.appengine.api.datastore.Entity;
 
 @WebServlet("/inLocation")
 public class IncomingLocationServlet extends HttpServlet{
-
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        
-    }
-
+    
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        
+
+     // Creates a location Entity and saves it to datastore and redirects to index.html 
+        Entity location = LocationService.createLocationEntity(request);
+        LocationService.save(location);
+        response.sendRedirect("index.html");
     }
 }
