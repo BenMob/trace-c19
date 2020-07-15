d6727ca10533be537ef730c76c931101436ebefd
angelicaharris <aharris@sps-program.com>
2020-07-15 00:12:17 -0500

    updates to login

--- trace-C19/src/main/java/com/tracecovid/services/ValidationService.java
+++ trace-C19/src/main/java/com/tracecovid/services/ValidationService.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 
 public class ValidationService {
-    public static boolean checkUser(String email,String username,String pass) 
+    public static boolean checkUser(String email,String username) 
     {
         boolean st =false;
         try {
@@ -14,8 +14,55 @@ public class ValidationService {
 
             //creating connection with the database
             //34.66.144.100
-            Connection con = DriverManager.getConnection("jdbc:mysql://34.66.144.100:3306","application","password123");
-            PreparedStatement ps = con.prepareStatement("select * from users where (email=? or username=?) and pass=?");
+            Connection con = DriverManager.getConnection("jdbc:mysql://34.66.144.100:3306/applicationDatabase","application","password123");
+            PreparedStatement ps = con.prepareStatement("select * from users where (email like ? or username like ?)");
+            ps.setString(1, email);
+            ps.setString(2, username);
+            ResultSet rs =ps.executeQuery();
+            st = rs.next();
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return st;                 
+    }   
+    public static boolean createUser(String email,String username,String password) 
+    {
+        boolean st =false;
+        try {
+
+            //loading drivers for mysql
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //creating connection with the database
+            //34.66.144.100
+            Connection con = DriverManager.getConnection("jdbc:mysql://34.66.144.100:3306/applicationDatabase","application","password123");
+            PreparedStatement ps = con.prepareStatement("insert into users (`email`, `username`, `password`) values (?,?,sha(?))");
+            ps.setString(1, email);
+            ps.setString(2, username);
+            ps.setString(3, password);
+            int rs =ps.executeUpdate();
+            st=(rs>0);
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return st;                 
+    }   
+    public static boolean login(String email,String username,String pass) 
+    {
+        boolean st =false;
+        try {
+
+            //loading drivers for mysql
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //creating connection with the database
+            //34.66.144.100
+            Connection con = DriverManager.getConnection("jdbc:mysql://34.66.144.100:3306/applicationDatabase","application","password123");
+            PreparedStatement ps = con.prepareStatement("select * from users where (email like ? or username like ?) and password=SHA(?)");
             ps.setString(1, email);
             ps.setString(2, username);
             ps.setString(3, pass);
@@ -28,4 +75,5 @@ public class ValidationService {
         }
         return st;                 
     }   
+
 }
--- trace-C19/src/main/java/com/tracecovid/servlets/UserServlet.java
+++ trace-C19/src/main/java/com/tracecovid/servlets/UserServlet.java
@@ -3,43 +3,61 @@ package com.tracecovid.servlets;
 
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
 import com.tracecovid.services.ValidationService;
 
-@WebServlet("/add-user")
+@WebServlet(name = "UserServlet", urlPatterns ="/add-user")
 public class UserServlet extends HttpServlet{
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().print("Testing");
+    }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //check valid password
-        //check valid user
-        //add user to database
+       
         //aharris-sps-summer20:us-central1:trace-covid
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         
+        String NewEmail= request.getParameter("NewEmail");
         String NewUser= request.getParameter("NewUser");
         String NewPass= request.getParameter("NewPass");
-        
-        if(ValidationService.checkUser("", NewUser, NewPass))
-        {
-            RequestDispatcher rs = request.getRequestDispatcher("Welcome");
-            rs.forward(request, response);
+        String NewPassConfirm= request.getParameter("NewPassConfirmation");
+
+       
+
+        if(!NewPass.equals(NewPassConfirm)){
+            out.println("Passwords do not match");
+           RequestDispatcher rs = request.getRequestDispatcher("index.html");
+           rs.include(request, response);
+           return;
         }
-        else
+        if(ValidationService.checkUser(NewEmail, NewUser)) 
         {
-           out.println("Username or Password incorrect");
+            out.println("Email or username already taken");
            RequestDispatcher rs = request.getRequestDispatcher("index.html");
            rs.include(request, response);
         }
-
+        else
+        {
+            if(ValidationService.createUser(NewEmail, NewUser, NewPass)){
+                out.println("User was created successfully");
+                RequestDispatcher rs = request.getRequestDispatcher("index.html");
+           rs.include(request, response);
+           return;
+            }
+           out.println("Error creating user");
+           RequestDispatcher rs = request.getRequestDispatcher("index.html");
+           rs.include(request, response);
+        }
     }
 }
 
