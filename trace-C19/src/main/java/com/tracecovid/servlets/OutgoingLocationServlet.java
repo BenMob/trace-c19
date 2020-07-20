d6b204eb88486333bb70ff00530d7d3414acfe83
Ben <benjaminombeni3@gmail.com>
2020-07-20 14:58:58 -0700

    Change List to ArrayList

--- trace-C19/src/main/java/com/tracecovid/servlets/OutgoingLocationServlet.java
+++ trace-C19/src/main/java/com/tracecovid/servlets/OutgoingLocationServlet.java
@@ -4,7 +4,7 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.tracecovid.services.LocationService;
 import com.tracecovid.entities.LocationModel;
 import java.io.IOException;
-import java.util.List;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,10 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 public class OutgoingLocationServlet extends HttpServlet{
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        List<LocationModel> locations = LocationService.getLocationsByZipCode(DatastoreServiceFactory.getDatastoreService(), request.getParameter("zip"));
+        final String zipCode = request.getParameter("zip");
+        final ArrayList<LocationModel> locations = LocationService.getLocationsByZipCode(zipCode);
 
-        //Prints Query results log on the console
-        System.out.println("\n\n\n--- QUERYING ZIP: " + request.getParameter("zip"));
+        // Prints Query results log on the console
+        System.out.println("\n\n\n--- QUERYING ZIP: " + zipCode);
         locations.forEach(loc -> {
             System.out.println(loc.toString());
         });
